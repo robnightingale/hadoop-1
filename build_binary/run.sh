51601240b3ffcf1884e2b478617583e817be4b98
Sumit Maji <skmaji1@outlook.com>
2018-06-21 10:34:06 +0530

    Commit

--- build_binary/run.sh
+++ build_binary/run.sh
@@ -3,4 +3,6 @@
 # The container will run as long as the script is running, that's why
 # we need something long-lived here
 #exec tail -f /var/log/tomcat6/catalina.out
+docker stop build
+docker rm build
 docker run -it -d --name build sumit/build /bin/bash
